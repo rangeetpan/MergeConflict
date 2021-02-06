39f5eed1f74e0dd8fda3d8c62ba7f2ec47002703
Pan <rangeet@iastate.edu>
2021-02-06 11:23:00 -0600

    test

--- DslSamples/MergeConflictsResolution/MergeConflictsResolution.Tests/Tests.cs
+++ DslSamples/MergeConflictsResolution/MergeConflictsResolution.Tests/Tests.cs
@@ -19,39 +19,80 @@ namespace MergeConflictsResolution
             return list.AsReadOnly();
         }
         [TestMethod]
-        public void TestLearnSubstringPositiveAbsPos()
+        public void Example1c1d()
         {
-            List<string> uppath = new List<string>();
-            List<string> downpath = new List<string>();
+            List<string> forked = new List<string>();
+            List<string> main = new List<string>();
             List<string> upfilepath = new List<string>();
             List<string> downfilepath = new List<string>();
             List<string> resolved = new List<string>();
-            List<string> uppath2 = new List<string>();
-            List<string> downpath2 = new List<string>();
+            List<string> forked2 = new List<string>();
+            List<string> main2 = new List<string>();
             List<string> upfilepath2 = new List<string>();
             List<string> downfilepath2 = new List<string>();
             List<string> resolved2 = new List<string>();
-            uppath.Add("A");
-            uppath.Add("C");
-            //downpath.Add("B");
-            resolved.Add("C");
-            //resolved.Add("B");
-            uppath2.Add("D");
-            uppath2.Add("E");
-            //downpath2.Add("B");
-            resolved2.Add("E");
-            MergeConflict input = new MergeConflict(PathToNode(uppath), PathToNode(downpath), PathToNode(upfilepath), PathToNode(upfilepath), "");
+            forked.Add("base/command_line.h");
+            forked.Add("base/logging.h");
+            main.Add("base/check_op.h");
+            resolved.Add("base/command_line.h");
+            resolved.Add("base/check_op.h");
+            forked2.Add("base/logging.h");
+            forked2.Add("base/scoped_native_library.h");
+            main2.Add("base/notreached.h");
+            resolved2.Add("base/scoped_native_library.h");
+            resolved2.Add("base/notreached.h");
+            MergeConflict input = new MergeConflict(PathToNode(forked), PathToNode(main), PathToNode(upfilepath), PathToNode(upfilepath), "");
             IReadOnlyList<Node> output = PathToNode(resolved);
-            MergeConflict input2 = new MergeConflict(PathToNode(uppath2), PathToNode(downpath2), PathToNode(upfilepath), PathToNode(upfilepath), "");
+            MergeConflict input2 = new MergeConflict(PathToNode(forked2), PathToNode(main2), PathToNode(upfilepath), PathToNode(upfilepath), "");
             IReadOnlyList<Node> output2 = PathToNode(resolved2);
             List<MergeConflictsResolutionExample> examples = new List<MergeConflictsResolutionExample>();
             examples.Add(new MergeConflictsResolutionExample(input, output));
             examples.Add(new MergeConflictsResolutionExample(input2, output2));
             Program program = Learner.Instance.Learn(examples);
             string s = program.Serialize();
-            System.IO.File.WriteAllText("1.txt", s);
+            System.IO.File.WriteAllText("Program1c1d.txt", s);
             IReadOnlyList<Node> outputProg = program.Run(input);
             Assert.AreEqual(true, Equal(output,outputProg));
+            IReadOnlyList<Node> test = program.Run(input);
+            IReadOnlyList<Node> outputProg2 = program.Run(input2);
+            Assert.AreEqual(true, Equal(output2, outputProg2));
+            ///program.Run(...);
+        }
+        [TestMethod]
+        public void Example1a1b()
+        {
+            List<string> forked = new List<string>();
+            List<string> main = new List<string>();
+            List<string> upfilepath = new List<string>();
+            List<string> downfilepath = new List<string>();
+            List<string> resolved = new List<string>();
+            List<string> forked2 = new List<string>();
+            List<string> main2 = new List<string>();
+            List<string> upfilepath2 = new List<string>();
+            List<string> downfilepath2 = new List<string>();
+            List<string> resolved2 = new List<string>();
+            forked.Add("ui/base/anonymous_ui_base_features.h");
+            forked.Add("ui/base/mojom/cursor_type.mojom-shared.h");
+            main.Add("ui/base/cursor/mojom/cursor_type.mojom-shared.h");
+            resolved.Add("ui/base/anonymous_ui_base_features.h");
+            resolved.Add("ui/base/mojom/cursor_type.mojom-shared.h");
+            forked2.Add("ui/base/anonymous_ui_base_feature.h");
+            forked2.Add("ui/base/mojom/cursor_type.mojom-blink.h");
+            main2.Add("ui/base/cursor/mojom/cursor_type.mojom-blink.h");
+            resolved2.Add("ui/base/anonymous_ui_base_feature.h");
+            resolved2.Add("ui/base/mojom/cursor_type.mojom-blink.h");
+            MergeConflict input = new MergeConflict(PathToNode(forked), PathToNode(main), PathToNode(upfilepath), PathToNode(upfilepath), "");
+            IReadOnlyList<Node> output = PathToNode(resolved);
+            MergeConflict input2 = new MergeConflict(PathToNode(forked2), PathToNode(main2), PathToNode(upfilepath), PathToNode(upfilepath), "");
+            IReadOnlyList<Node> output2 = PathToNode(resolved2);
+            List<MergeConflictsResolutionExample> examples = new List<MergeConflictsResolutionExample>();
+            examples.Add(new MergeConflictsResolutionExample(input, output));
+            examples.Add(new MergeConflictsResolutionExample(input2, output2));
+            Program program = Learner.Instance.Learn(examples);
+            string s = program.Serialize();
+            System.IO.File.WriteAllText("Program1a1b.txt", s);
+            IReadOnlyList<Node> outputProg = program.Run(input);
+            Assert.AreEqual(true, Equal(output, outputProg));
             IReadOnlyList<Node> test = program.Run(input);
             IReadOnlyList<Node> outputProg2 = program.Run(input2);
             Assert.AreEqual(true, Equal(output2, outputProg2));
