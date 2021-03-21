0fa796d824238e6b2c10f00edc7841fb44b014f1
Pan <rangeet@iastate.edu>
2021-03-21 12:48:47 -0500

    sample dataset

--- MergeConflictsResolution/MergeConflictsResolutionConsole/SampleDataset.cs
+++ MergeConflictsResolution/MergeConflictsResolutionConsole/SampleDataset.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using MergeConflictsResolution;
 using Microsoft.ProgramSynthesis.Wrangling.Tree;
-using NUnit.Framework;
+//using NUnit.Framework;
 using static MergeConflictsResolution.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace MergeConflictsResolutionConsole
 {
-    class SampleDataset
+    [TestClass]
+    public class SampleDataset
     {
+        [TestMethod]
         public void EntireTest()
         {
             List<bool> checkOutput = new List<bool>();
@@ -22,9 +25,10 @@ namespace MergeConflictsResolutionConsole
                 checkOutput.Add(ValidOutput(input, programList, testcasePath, number));
             }
             List<bool> valid = checkOutput;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(true, true);
         }
-        [Test]
-        [TestCase("1")]
+        //[Test]
+        //[TestCase("1")]
         public void IncludeTest(string fileName)
         {
             List<bool> checkOutput = new List<bool>();
